0483b0a58536fc3de129347334540bdd9c719697
mainak55512 <mbhattacharjee432@gmail.com>
2025-12-28 22:06:15 +0530

    added doc comments

--- include/cstring.h
+++ include/cstring.h
@@ -8,25 +8,54 @@
 
 typedef struct Arena Arena;
 
-void *arena_alloc(Arena *arena, size_t size);
-Arena *arena_init(size_t capacity);
-void arena_free(Arena **arena);
+/* Allocates memory on Heap with maximum memory alignment */
 void *m_align_alloc(size_t capacity);
+
+/* Frees memory allocated by m_align_alloc or m_align_realloc */
 void m_align_free(void *ptr);
+
+/* Re-allocates memory on Heap with maximum memory alignment */
 void *m_align_realloc(void *ptr, size_t old_size, size_t new_size);
+
+/* Initiates an arena with the given capacity */
+Arena *arena_init(size_t capacity);
+
+/* Allocates a chunk on the arena, when the arena
+ * gets full, it calls `arena_init` again internally to allocate more memory */
+void *arena_alloc(Arena *arena, size_t size);
+
+/* Frees the arena and all the allocations done on it */
+void arena_free(Arena **arena);
 
 #endif // ARENA_H
 
 #include <stdio.h>
 
 typedef struct String String;
+
+/* Creates a String object from c-string (char *) */
 String *string_from(Arena *arena, char *str);
+
+/* Concatinates two string objects */
 String *string_concat(Arena *arena, String *str1, String *str2);
+
+/* Returns the length of the string */
 int string_len(String *str);
+
+/* Returns a substring from a String where `begin` is inclusive but `end` is
+ * exclusive */
 String *string_sub(Arena *arena, String *str, int begin, int end);
+
+/* Returns the c-string (char *) from the String object */
 char *string(String *st);
+
+/* Concatinates two c-strings (char *) */
 String *string_concat_cstr(Arena *arena, char *s1, char *s2);
+
+/* Trims all leading and trailing white-spaces from the String */
 String *string_trim(Arena *arena, String *str);
+
+/* Takes String input from console */
 String *string_get(Arena *arena);
 
 #endif // CSTRING_H
